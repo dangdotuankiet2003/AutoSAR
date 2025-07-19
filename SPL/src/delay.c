6f34480030590b98f2f0986a4b9d411baa2383d2
dangdotuankiet2003 <101450681+dangdotuankiet2003@users.noreply.github.com>
2025-07-19 15:56:17 +0700

    Delete SPL/src directory

--- SPL/src/delay.c
+++
@@ -1,9 +0,0 @@
-#include "stm32f10x.h"
-#include "stm32f10x_tim.h"
-#include "stm32f10x_rcc.h"
-#include "delay.h"
-
-void Delay(uint32_t ms) {
-    uint32_t i;
-    for (i = 0; i < ms * 8000; i++);
-}
